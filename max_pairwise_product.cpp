aaafbd321b1c4285681bc963d9ffec0ea18536c6
Juan Ureña <juanurenagarciagasco@gmail.com>
2020-07-18 19:26:22 +0200

    Añadida función más rapida y comparativa de tiempos

--- max_pairwise_product.cpp
+++ max_pairwise_product.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <ctime>
+
+
+long long FastMaxPairwiseProduct(const std::vector<int>& numbers) {
+    long long max_product = 0;
+    int n = numbers.size();
+
+    int First=-1;
+    int Second=-1;
+
+    for (int i = 0; i < n; ++i) {
+      if (First==-1 || numbers[i]>numbers[First]){
+        First=i;
+      }
+    }
+
+    for (int j = 0; j < n; ++j) {
+      if (j!=First && (Second==-1 || numbers[j]>numbers[Second])){
+        Second=j;
+      }
+    }
+
+    return (long long)(numbers[First])*numbers[Second];
+}
+
 
 long long MaxPairwiseProduct(const std::vector<int>& numbers) {
     long long max_product = 0;
@@ -17,13 +42,32 @@ long long MaxPairwiseProduct(const std::vector<int>& numbers) {
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> numbers[i];
-    }
 
+//Entrada estandar
+
+    // int n;
+    // std::cin >> n;
+    // std::vector<int> numbers(n);
+    // for (int i = 0; i < n; ++i) {
+    //     std::cin >> numbers[i];
+    // }
+
+//Medición de los tiempos de computo
+    unsigned t0, t1, t2, t3;
+
+//Entradas para test de los tiempos
+    std::vector<int> numbers(50000, 0);
+    t0=clock();
     std::cout << MaxPairwiseProduct(numbers) << "\n";
+    t1 = clock();
+    double timeSlow = (double(t1-t0)/CLOCKS_PER_SEC);
+    std::cout << "Tiempo de ejecución Slow:"  << timeSlow <<"\n";
+
+    t2=clock();
+    std::cout << FastMaxPairwiseProduct(numbers) << "\n";
+    t3 = clock();
+    double timeFast = (double(t3-t2)/CLOCKS_PER_SEC);
+    std::cout << "Tiempo de ejecución Fast:"  << timeFast <<"\n";
+
     return 0;
 }
